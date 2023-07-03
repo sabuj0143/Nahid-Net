0f569cf85f0974a96b8813f621d39e827205f5d8
sabuj0143 <sabujhowlader011@gmai.com>
2023-07-03 20:30:16 +0600

    More update

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -40,9 +40,9 @@ const Banner = () => {
           }}
           data-swiper-parallax="-23%"
         ></div>
-        <SwiperSlide>
+        <SwiperSlide className="md:mt-10 md:ml-8">
           <div className="title" data-swiper-parallax="-300">
-            <h1> It's Going Fast.</h1>
+            <h1 className="font-bold"> It's Going Fast.</h1>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
             <h4>Super-first Internet + No Contracts</h4>
@@ -51,52 +51,43 @@ const Banner = () => {
             <h4 className="uppercase">Internet 100 for</h4>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            <h4 className="uppercase">$ 39.99 <span className="lowercase">/mo</span></h4>
+            <h4 className="uppercase font-bold">$39.99 <span className="lowercase">/mo</span></h4>
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>For a limited time, double the speed for just $5 More.</p>
           </div>
           <div className="text" data-swiper-parallax="-100">
-          <button className="btn btn-info sm:btn-sm md:btn-md lg:btn-md mt-4">get offer now <FaArrowRight className="ml-2"/></button>
+            <button className="btn btn-info sm:btn-sm md:btn-md lg:btn-md mt-4">get offer now <FaArrowRight className="ml-2" /></button>
           </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 2
+        <SwiperSlide className="md:mt-10 md:ml-8">
+          <div className="subtitle" data-swiper-parallax="-200">
+            <h4>Switch to internet Service you will love</h4>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
+            <h4 className="uppercase">Internet 100 for</h4>
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            <h4 className="uppercase font-bold">$39.99 <span className="lowercase">/mo</span></h4>
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>For a limited time, double the speed for just $5 More.</p>
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <button className="btn btn-info sm:btn-sm md:btn-md lg:btn-md mt-4">get offer now <FaArrowRight className="ml-2" /></button>
+          </div>
+          {/* <div className="subtitle" data-swiper-parallax="-200">
+            <h1 className="text-2xl font-bold mb-6">Switch to internet Service you will love</h1>
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              This is the place to learn about Nahid.net internet the high speed, and reliable internet collocation that lets you do what online.
             </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
           </div>
           <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
-          </div>
+            <button className="btn btn-info sm:btn-sm md:btn-md lg:btn-md mt-4">get offer now <FaArrowRight className="ml-2" /></button>
+            <button className="btn btn-info sm:btn-sm md:btn-md lg:btn-md mt-4">get <FaArrowRight className="ml-2" /></button>
+          </div> */}
         </SwiperSlide>
       </Swiper>
     </>
